cdd72150295997215863b8c9a18799ec73ce18e4
raifashafi <raifafisat@gmail.com>
2024-06-12 16:03:46 +0530

    project added

--- app.js
+++ app.js
@@ -43,6 +43,19 @@ app.post("/search",(req,res)=>{
             }
     )
 })
-app.listen(8082,()=>{
+app.post("/delete",(req,res)=>
+    {
+ let input=req.body
+ coursemodel.findByIdAndDelete(input._id).then(
+    (response)=>{
+        res.json({"status":"success"})
+    }
+ ).catch(
+    (error)=>{
+        res.json({"status":"error"})
+    }
+ )
+    })
+app.listen(8084,()=>{
     console.log("server started")
 })
